480ef17d22cc421ac21f3b4c4b181b04bb747375
Shubham Puhal <shubhampuhal01@gmail.com>
2024-03-25 03:02:52 +0530

    SplashPage TypedText Title added.

--- src/Components/SplashPage Components/TypedText.jsx
+++ src/Components/SplashPage Components/TypedText.jsx
@@ -6,25 +6,72 @@ function TypedText() {
 
     const [displayTextPre, setDisplayTextPre] = useState('');
     const [displayTextPost, setDisplayTextPost] = useState('');
-
     const [showCurser, setShowCursor] = useState(true);
 
-    const TargetText = 'console.log();';
-    const CompleteText = 'console.log("happiness");'
+
+    const logWord = "Portfolio";
+
+    const text = 'console.log();';
+    const speed = 400;
+
+
+    // 3 stages: stage1-> console.log(); : stage2-> console.log(""_); : stage3-> console.log("happiness_");
     useEffect(() => {
-        const timer = setInterval(() => {
-            setShowCursor(!showCurser);
-            setDisplayTextPre(prev => prev + TargetText[prev.length]);
-            if (displayTextPre === TargetText) {
-                setDisplayTextPre('');
+        let curserBool = true;
+        let index = 0;
+        let iterate = true;
+        let i2 = 0;
+        let logWordIndex = 0;
+
+        const interval = setInterval(() => {
+            curserBool = !curserBool;
+            if (index < text.length) {
+                setDisplayTextPre(text.substring(0, index + 1));
+            } else if (index === text.length) {
+                iterate = false;
+                console.log(i2);
+                console.log(logWordIndex);
+                if(i2 == 0){
+                }
+                else if (i2 == 1) {
+                    setDisplayTextPost(";");
+                    setDisplayTextPre('console.log()');
+                }
+                else if (i2 == 2) {
+                    setDisplayTextPost(");");
+                    setDisplayTextPre('console.log(');
+                }
+                else if (i2 == 3) {
+
+                }
+                else if (i2 == 4) {
+                    setDisplayTextPost('");');
+                    setDisplayTextPre('console.log("');
+                }
+                else {
+                    if (logWordIndex < logWord.length) {
+                        setDisplayTextPre(prev => prev + logWord[logWordIndex]);
+                        logWordIndex = logWordIndex + 1;
+                    }
+
+                }
+
             }
-        }, 300);
-        return () => clearInterval(timer);
-    }, [displayTextPre, TargetText]);
+            setShowCursor(curserBool);
+            if (iterate) {
+                index = index === text.length ? index - 1 : index + 1;
+            }
+            else {
+                i2 = i2 + 1;
+            }
+        }, speed);
+
+        return () => clearInterval(interval);
+    }, [text, speed]);
 
     return (
-        <div>
-            <h1 className='text-3xl text-left font-bold'>{displayTextPre}{showCurser ? "|" : ""}{displayTextPost}</h1>
+        <div className='flex justify-center items-center min-h-screen'>
+            <h1 className='text-6xl text-left font-bold'>{displayTextPre}{showCurser ? "|" : " "}{displayTextPost}</h1>
         </div>
     )
 }
